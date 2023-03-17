e6a5b102f3d7a63c8127fe7c10bbfab273f82b27
Michael Quarcoo <michaelquarcoo04@gmail.com>
2023-03-17 23:04:14 +0000

    Header component added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Restaurants Nearby</h1>
+      <p>Find the best location to wine and dine</p>
+    </header>
+  );
+};
+
+export default Header;
